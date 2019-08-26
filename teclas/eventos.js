39668ee6c35772b2a3816181f58ae08945169e37
Ninisbeth Segovia Noguera <ninisbeth_segovia@hotmail.com>
2019-08-25 21:57:59 -0500

    creado metodo de detectar la tecla oprimida

---
+++ teclas/eventos.js
@@ -0,0 +1,13 @@
+var teclas = 
+{
+    UP:38,
+    DOWN:40,
+    LEFT:37,
+    RIGHT:39
+};
+
+document.addEventListener("keyup",dibujarTeclado);
+function dibujarTeclado(eventox)
+{
+    console.log("tecla oprimida");
+}
